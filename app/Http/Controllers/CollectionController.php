d7ed91983afb80bec305861ba208d12fd19e9bfe
Koichi.Shimomura <unieye51@gmail.com>
2022-06-01 22:54:46 +0900

    1-2 What is collection

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class CollectionController extends Controller
 {
     public function index()
     {
-        return "hello world";
+        $data = [
+            [
+                'name' => 'ahmed',
+                'score' => 30,
+                'exam' => [
+                    'math' => '10',
+                    'english' => '20'
+                ]
+            ],
+            [
+                'name' => 'ali',
+                'score' => 20,
+                'exam' => [
+                    'math' => '12',
+                    'english' => '25'
+                ]
+            ],
+        ];
+        $collection = collect($data);
+
+        return $collection->max('exam.math');
+
+//        $exam = array_column($data, 'exam');
+//        $math = array_column($exam, 'math');
+//
+//        return max($math);
     }
 }
